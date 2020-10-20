e3f8834f1e447806a938ac5b30c1319c0369eb74
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-20 17:29:45 +0000

    Deploy SArkar-Htet/lists-and-keys to github.com/SArkar-Htet/lists-and-keys.git:gh-pages

---
+++ precache-manifest.30debc37d8ab9e3f00b009c7a64c57b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5de2a25bc963187473300c813da7bd",
+    "url": "/lists-and-keys/index.html"
+  },
+  {
+    "revision": "d63ec00633f2df14094e",
+    "url": "/lists-and-keys/static/css/main.e3d7f5f4.chunk.css"
+  },
+  {
+    "revision": "62b3f4e3669d60f47f0a",
+    "url": "/lists-and-keys/static/js/2.225a396e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lists-and-keys/static/js/2.225a396e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63ec00633f2df14094e",
+    "url": "/lists-and-keys/static/js/main.a7d70b19.chunk.js"
+  },
+  {
+    "revision": "2f9d65e8c55ecb279b78",
+    "url": "/lists-and-keys/static/js/runtime-main.ae2143bd.js"
+  }
+]);
